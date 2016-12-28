997e778699ea15a8082f575c48dff5d57a09586b
Jonas Drotleff <jonas.drotleff@gmail.com>
2016-12-28 20:32:22 +0100

    read and write stores

--- src/main/java/com/fidelio/processing/io/State.java
+++ src/main/java/com/fidelio/processing/io/State.java
@@ -1,8 +1,18 @@
 package com.fidelio.processing.io;
 
+import com.fidelio.processing.objects.Distance;
 import com.fidelio.processing.objects.Star;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -12,18 +22,31 @@ import java.util.List;
  */
 public class State {
 
+    private static final String KEY_ID = "id";
+    private static final String KEY_NAME = "name";
+    private static final String KEY_DATE = "date";
+    private static final String KEY_FILE = "file";
+    private static final String KEY_STARS = "stars";
+    private static final String KEY_DISTANCES = "distances";
+
     private String id;
     private String name;
     private Date date;
     private File file;
     private List<Star> stars = new ArrayList<Star>();
+    private List<Distance> distances = new ArrayList<Distance>();
 
-    public State(String id, String name, Date date, File file, List<Star> stars) {
+    public State(String id, String name, Date date, File file, List<Star> stars, List<Distance> distances) {
         this.id = id;
         this.name = name;
         this.date = date;
         this.file = file;
         this.stars = stars;
+        this.distances = distances;
+    }
+
+    public State() {
+
     }
 
     public String getId() {
@@ -64,6 +87,74 @@ public class State {
 
     public void setStars(List<Star> stars) {
         this.stars = stars;
+    }
+
+    public List<Distance> getDistances() {
+        return distances;
+    }
+
+    public void setDistances(List<Distance> distances) {
+        this.distances = distances;
+    }
+
+    public Element getElementDOM() throws ParserConfigurationException {
+        DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+
+        Document doc = docBuilder.newDocument();
+        Element state = doc.createElement("state");
+        state.setAttribute(KEY_ID, id);
+        doc.appendChild(state);
+
+        Element name = doc.createElement(KEY_NAME);
+        name.setTextContent(this.name);
+        state.appendChild(name);
+
+        Element date = doc.createElement(KEY_DATE);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("GMT");
+        date.setTextContent(dateFormat.format(this.date));
+        state.appendChild(date);
+
+        Element file = doc.createElement(KEY_FILE);
+        file.setTextContent(this.file.toString());
+        state.appendChild(file);
+
+        Element stars = doc.createElement(KEY_STARS);
+        state.appendChild(stars);
+
+        for (int i = 0; i < this.stars.size(); i++) {
+            state.appendChild(this.stars.get(i).getElementDOM());
+        }
+
+        Element distances = doc.createElement(KEY_DISTANCES);
+        state.appendChild(distances);
+
+        for (int i = 0; i < this.distances.size(); i++) {
+
+        }
+
+        return state;
+    }
+
+    public void loadElementDOM(Element element) {
+        this.id = element.getAttribute(KEY_ID);
+        this.name = element.getElementsByTagName(KEY_NAME).item(0).getTextContent();
+
+        SimpleDateFormat format = new SimpleDateFormat("GMT");
+        try {
+            this.date = format.parse(element.getElementsByTagName(KEY_DATE).item(0).getTextContent());
+        } catch (Exception e) {
+            // TODO error while parsing date
+            e.printStackTrace();
+        }
+        this.file = new File(element.getElementsByTagName(KEY_FILE).item(0).getTextContent());
+
+        NodeList nodeList = element.getElementsByTagName(KEY_STARS).item(0).getChildNodes();
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            Star star = new Star();
+            star.loadElementDOM((Element) nodeList.item(i));
+            stars.add(star);
+        }
     }
 
     /* TODO:
--- src/main/java/com/fidelio/processing/io/Store.java
+++ src/main/java/com/fidelio/processing/io/Store.java
@@ -1,16 +1,37 @@
 package com.fidelio.processing.io;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import sun.security.krb5.internal.PAData;
+
+import javax.print.Doc;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by jonasdrotleff on 27.12.16.
+ * Created by jonas drotleff on 27.12.16.
  */
 public class Store {
 
+
+    private static final String FILE = // ~/.fidelio/store.xml
+            System.getProperty("user.home")
+            + File.separatorChar
+            + ".fidelio"
+            + File.separatorChar
+            + "store.xml";
     private File file;
     private List<State> states = new ArrayList<State>();
+    private Document doc;
 
     public Store(File file) {
         this.file = file;
@@ -28,11 +49,81 @@ public class Store {
     }
 
     public void write() {
+        clean();
+        createNew(file);
+    }
+
+    public void clean() {
 
     }
 
     public void read() {
+        if (file.exists()) {
+            try {
+                DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+                DocumentBuilder db = dbf.newDocumentBuilder();
+                doc = db.parse(file);
 
+                doc.getDocumentElement().normalize();
+
+                NodeList nList = doc.getElementsByTagName("saved-states");
+
+                for (int i = 0; i < nList.getLength(); i++) {
+                    State state = new State();
+                    state.loadElementDOM((Element) nList.item(i));
+                    states.add(state);
+                }
+
+            } catch (Exception e) {
+                // TODO error reading file
+                e.printStackTrace();
+            }
+        } else {
+            // TODO file does not exist
+            return;
+        }
+    }
+
+    public File createNew(File file) {
+        if (file.exists()) {
+            // TODO ask to override file
+        } else {
+            if (!new File(file.getParent()).exists()) {
+                try {
+                    new File(file.getParent()).mkdir();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    // TODO could not create directory
+                }
+            }
+
+            try {
+                DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+                DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+
+                doc = docBuilder.newDocument();
+                Element states = doc.createElement("saved-states");
+                doc.appendChild(states);
+
+                for (int i = 0; i < this.states.size(); i++) {
+                    Element state = this.states.get(i).getElementDOM();
+                    states.appendChild(state);
+                }
+
+                TransformerFactory transformerFactory = TransformerFactory.newInstance();
+                Transformer transformer = transformerFactory.newTransformer();
+                DOMSource source = new DOMSource(doc);
+                StreamResult result = new StreamResult(this.file);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return file;
+    }
+
+    public File createNew() {
+        return createNew(new File(FILE));
     }
 
     /* TODO:
