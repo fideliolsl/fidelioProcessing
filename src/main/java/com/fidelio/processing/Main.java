9b8acbf15f5125dcd066ce73aee888a76b83ea16
Jonas Drotleff <jonas.drotleff@gmail.com>
2016-12-28 20:33:40 +0100

    implementing the store

--- src/main/java/com/fidelio/processing/Main.java
+++ src/main/java/com/fidelio/processing/Main.java
@@ -1,10 +1,13 @@
 package com.fidelio.processing;
 
+import com.fidelio.processing.io.Store;
+
 /**
  * Created by Jonas Drotleff on 27.12.16.
  */
 public class Main {
     public static void main(String[] args) {
-
+        Store store = new Store();
+        store.createNew();
     }
 }
